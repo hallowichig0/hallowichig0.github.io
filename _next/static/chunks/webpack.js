294f1e6f51b3074a8385cf47a57890f40b4eba8d
Halcyon-PC14 <garciajayson502@gmail.com>
2022-06-20 23:52:58 +0800

    Fixed header & footer logo not animate to top

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8f833cb73bf1fb7ef5ff";
+/******/ 	var hotCurrentHash = "c9ff85e0046bb2dad68b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
