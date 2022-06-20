f0079105fcd001aaf2beec23f21ad445f3307eba
Halcyon-PC14 <garciajayson502@gmail.com>
2022-06-20 23:13:41 +0800

    Update mobile device

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9c04464e3343660f01f8";
+/******/ 	var hotCurrentHash = "d2c988d48212d9a085fe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
