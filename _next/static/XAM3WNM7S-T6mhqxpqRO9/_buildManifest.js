46218a5af979506ee8c7cc4894a6147391bef5e4
Halcyon-PC14 <garciajayson502@gmail.com>
2022-06-20 23:39:07 +0800

    Fix header overlap

--- _next/static/XAM3WNM7S-T6mhqxpqRO9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F7184a3f9.0ae83b82f4b4f865edce.js","static\u002Fchunks\u002Fpages\u002Findex-468a51c34c1c207f5376.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e880db3e6399bc724986.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d2c988d48212d9a085fe";
+/******/ 	var hotCurrentHash = "8f833cb73bf1fb7ef5ff";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
