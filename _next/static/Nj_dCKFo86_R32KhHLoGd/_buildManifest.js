e10fc8c7d35645d544548057c0bd975377b0e540
Jayson Garcia <jaysongarcia@Jaysons-MacBook-Pro.local>
2022-06-29 23:00:10 +0800

    Update hero, experience & contact component

--- _next/static/Nj_dCKFo86_R32KhHLoGd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F7184a3f9.0ae83b82f4b4f865edce.js","static\u002Fchunks\u002Fpages\u002Findex-5ec83b9d9b63626f27ec.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85cfbb9abfbb27be676c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/fDhO8ykPGLodpCDqw3ZAM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F7184a3f9.0ae83b82f4b4f865edce.js","static\u002Fchunks\u002Fpages\u002Findex-43cfc85c8732a86335c2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85cfbb9abfbb27be676c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
